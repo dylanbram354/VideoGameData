b042a02d103280f0da706e15007cb229727d80df
dylanbram354 <dylanbram354@gmail.com>
2021-06-11 15:59:01 -0500

    Fixed error in game search function

--- Video_Game_Data/video_game_data_analysis/urls.py
+++ Video_Game_Data/video_game_data_analysis/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'video_game_data_analysis'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search_by_title/', views.search_by_title, name='search_by_title')
+    path('search_by_title/', views.search_by_title, name='search_by_title'),
+    path('compare_platforms/', views.compare_platforms, name='compare_platforms')
 ]
--- Video_Game_Data/video_game_data_analysis/views.py
+++ Video_Game_Data/video_game_data_analysis/views.py
@@ -40,27 +40,66 @@ def index(request):
 
 def search_by_title(request):
     if request.method == 'GET':
-        return render(request, 'search_by_title.html')
+        context = {'get': True}
+        return render(request, 'search_by_title.html', context)
     elif request.method == 'POST':
         search = request.POST.get('title')
         alphanumeric_search = [character for character in search if character.isalnum()]
         search = "".join(alphanumeric_search)
         all_games = requests.get('https://api.dccresource.com/api/games/').json()
-        found_game = 'none found'
+        found_game = None
         for game in all_games:
             alphanumeric = [character for character in game['name'] if character.isalnum()]
             alphanumeric_game = "".join(alphanumeric)
             if alphanumeric_game.upper() == search.upper():
-                if found_game != 'none found':
+                if found_game is not None:
                     found_game.append(game)
                 else:
                     found_game = [game]
-        platforms = []
-        sales = []
-        for entry in found_game:
-            platforms.append(entry['platform'])
-            sales.append(entry['globalSales'])
-        context = {'game': found_game,
-                   'platforms': platforms,
-                   'sales': sales}
+        if found_game is not None:
+            platforms = []
+            sales = []
+            for entry in found_game:
+                platforms.append(entry['platform'])
+                sales.append(entry['globalSales'])
+            context = {'game': found_game,
+                       'platforms': platforms,
+                       'sales': sales}
+        else:
+            context = {'game': None}
         return render(request, 'search_by_title.html', context)
+
+
+def compare_platforms(request):
+    years = []
+    i = 1980
+    while i <= 2021:
+        years.append(i)
+        i += 1
+    if request.method == "GET":
+        context = {'years': years}
+    else:
+        chosen_year = request.POST.get('year')
+        all_games = requests.get('https://api.dccresource.com/api/games/').json()
+        games_in_year = []
+        for entry in all_games:
+            if str(entry['year']) == chosen_year:
+                games_in_year.append(entry)
+        platform_releases = {}
+        for entry in games_in_year:
+            if entry['platform'] not in platform_releases:
+                platform_releases[entry['platform']] = 1
+            else:
+                platform_releases[entry['platform']] += 1
+        platforms = []
+        releases = []
+        for key in platform_releases:
+            platforms.append(key)
+            releases.append(platform_releases[key])
+        context = {'years': years,
+                   'year': chosen_year,
+                   'platforms': platforms,
+                   'releases': releases,
+                   'chart_data': True}
+    return render(request, 'compare_platforms.html', context)
+
